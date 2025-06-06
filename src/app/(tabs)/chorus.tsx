ba4b58f795efc06f640ddca973aedefe741944ca
JyrwaJR <jyrwaboys@gmail.com>
2025-06-06 22:42:11 +0530

    create chorus page

---
+++ src/app/(tabs)/chorus.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Text } from '~/src/components/ui/typography';
+
+export default function Chorus() {
+  return (
+    <View>
+      <Text>Chorus</Text>
+    </View>
+  );
+}
