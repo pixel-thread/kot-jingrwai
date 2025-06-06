9206c8194608035012d3073aa07fa332f2d52011
Bimon <bimonlangnongsiej@gmail.com>
2025-06-07 00:37:42 +0530

    feat(songs): add new song collections for God's Word and Holy Spirit
    Add two new song collections: 'shaphang_ka_ktien_u_blei' with 12 songs (129-141) about God's Word, and 'shaphang_u_mynsiem_bakhuid' with 14 songs (115-128) about the Holy Spirit. Also added 13 songs (102-114) about Christ's return to existing 'shaphang_u_khrist' collection.
    
    The new songs provide worship material focused on biblical themes of Scripture, the Holy Spirit's work, and the second coming of Christ. Each song includes complete metadata and lyrics in Khasi language.

--- src/libs/songs/index.ts
+++ src/libs/songs/index.ts
@@ -1,5 +1,7 @@
 import { SongT } from '~/src/types/song';
 import { shaphangUBlei } from './shaphang_u_blei';
 import { shaphangUKhrist } from './shaphang_u_khrist';
+import { shaphangUMynsiemBakhuid } from './shaphang_u_mynsiem_bakhuid';
+import { shaphangKaKtienUBlei } from './shaphang_ka_ktien_u_blei';
 
-export const songs: SongT[] = [...shaphangUBlei, ...shaphangUKhrist];
+export const songs: SongT[] = [...shaphangUBlei, ...shaphangUKhrist, ...shaphangUMynsiemBakhuid, ...shaphangKaKtienUBlei];
