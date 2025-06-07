428a82f62421dd99cd9ed1747c683016dbb1db93
Bimon <bimonlangnongsiej@gmail.com>
2025-06-07 11:25:06 +0530

    feat(songs): add shaphangKaBalang to songs export

--- src/libs/songs/index.ts
+++ src/libs/songs/index.ts
@@ -3,5 +3,6 @@ import { shaphangUBlei } from './shaphang_u_blei';
 import { shaphangUKhrist } from './shaphang_u_khrist';
 import { shaphangUMynsiemBakhuid } from './shaphang_u_mynsiem_bakhuid';
 import { shaphangKaKtienUBlei } from './shaphang_ka_ktien_u_blei';
+import { shaphangKaBalang } from './shaphang_ka_balang';
 
-export const songs: SongT[] = [...shaphangUBlei, ...shaphangUKhrist, ...shaphangUMynsiemBakhuid, ...shaphangKaKtienUBlei];
+export const songs: SongT[] = [...shaphangUBlei, ...shaphangUKhrist, ...shaphangUMynsiemBakhuid, ...shaphangKaKtienUBlei,...shaphangKaBalang];
