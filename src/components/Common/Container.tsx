6f61966f01da482c8cad640f4c4dd97ab1cfe1b2
JyrwaJR <jyrwaboys@gmail.com>
2025-06-15 22:46:17 +0530

    fix

--- src/components/Common/Container.tsx
+++ src/components/Common/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps extends ViewProps {
 }
 export const Container = ({ children, className }: ContainerProps) => {
   return (
-    <SafeAreaView className={cn(className, 'flex flex-1 bg-primary dark:bg-dark-primary')}>
+    <SafeAreaView className={cn(className, 'flex flex-1 bg-gray-200 dark:bg-gray-950')}>
       {children}
     </SafeAreaView>
   );
