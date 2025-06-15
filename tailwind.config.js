3bbf686e6d502fb576a35e93fd783f3e2bb2a561
JyrwaJR <jyrwaboys@gmail.com>
2025-06-15 23:26:06 +0530

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,tsx}'],
-  darkMode: 'class',
+  darkMode: 'class', // Enable class-based dark mode
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        liturgical: {
+          green: 'var(--liturgical-green)',
+          red: 'var(--liturgical-red)',
+          purple: 'var(--liturgical-purple)',
+          white: 'var(--liturgical-white)',
+        },
+      },
+    },
   },
   plugins: [],
 };
