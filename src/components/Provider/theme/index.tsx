2526aa3fc1e3bf2b9658b0f70c4a8b4ae9b2759e
JyrwaJR <jyrwaboys@gmail.com>
2025-06-18 22:16:57 +0530

    set theme color

--- src/components/Provider/theme/index.tsx
+++ src/components/Provider/theme/index.tsx
@@ -5,8 +5,11 @@ import { useThemeStore } from '~/src/libs/stores/theme';
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useThemeStore();
   const { setColorScheme } = useColorScheme();
+
   useEffect(() => {
-    setColorScheme(theme);
+    if (theme) {
+      setColorScheme(theme);
+    }
   }, [theme, setColorScheme]);
 
   return <>{children}</>;
