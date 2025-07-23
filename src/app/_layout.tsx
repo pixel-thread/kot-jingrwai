4d24c7a9a6cfa485563b091c99cb6a715f3073b4
JyrwaJR <jyrwaboys@gmail.com>
2025-07-23 15:46:58 +0530

    add splahscreen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,30 +4,69 @@ import { SongProvider } from '../components/Provider/Song';
 import { StatusBar } from 'react-native';
 import { CustomStack } from '../components/Common/CustomStack';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SplashScreen } from '../components/Common/SplashScreen';
 import { Ternary } from '../components/Common/Ternary';
 import { ThemeProvider } from '../components/Provider/theme';
 import AppVersion from '../components/Common/AppVersion';
 import { TQueryProvider } from '../components/Provider/query';
+import * as SplashScreen from 'expo-splash-screen';
+import { Stack } from 'expo-router';
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function Layout() {
-  const [isMounted, setIsMounted] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Remove this if you copy and paste the code!
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView onLayout={onLayoutRootView} style={{ flex: 1 }}>
       <SafeAreaProvider className="flex-1">
         <StatusBar className={'bg-gray-200 text-black dark:bg-gray-950 dark:text-gray-200'} />
         <ThemeProvider>
           <TQueryProvider>
             <SongProvider>
               <SafeAreaView className="flex-1 bg-gray-200 dark:bg-gray-950">
-                <Ternary
-                  condition={isMounted}
-                  ifTrue={<CustomStack />}
-                  ifFalse={<SplashScreen onFinish={() => setIsMounted(true)} />}
-                />
+                <Stack screenOptions={{ headerShown: false }} />
                 <AppVersion />
               </SafeAreaView>
             </SongProvider>
