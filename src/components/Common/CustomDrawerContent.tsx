faf77aa7833215abd4abf1ac6c7346b1d7925dc1
JyrwaJR <jyrwaboys@gmail.com>
2025-08-11 14:55:31 +0530

    fix route type

--- src/components/Common/CustomDrawerContent.tsx
+++ src/components/Common/CustomDrawerContent.tsx
@@ -12,31 +12,15 @@ import { useColorScheme } from 'nativewind';
 import colors from 'tailwindcss/colors';
 
 const menuItems: MenuItemsT[] = [
-  {
-    id: 1,
-    title: 'Home',
-    herf: '',
-  },
-  {
-    id: 2,
-    title: 'Apostle Creed',
-    herf: 'apostle-creed',
-  },
-  { id: 6, title: 'Tynrai Jingrwai', herf: 'tynrai-jingrwai' },
-  { id: 7, title: 'Shaphang Jongngi', herf: 'about' },
-  { id: 3, title: 'Contact', herf: 'contact' },
-  { id: 4, title: 'Report', herf: 'report' },
-  {
-    id: 8,
-    title: 'About',
-    herf: 'about',
-  },
-  {
-    id: 5,
-    title: 'Settings',
-    herf: 'setting',
-  },
+  { id: 1, title: 'Home', herf: '/' },
+  { id: 2, title: 'Apostle Creed', herf: '/apostle-creed' },
+  { id: 6, title: 'Tynrai Jingrwai', herf: '/tynrai-jingrwai' },
+  { id: 3, title: 'Contact', herf: '/contact' },
+  { id: 4, title: 'Report', herf: '/report' },
+  { id: 5, title: 'Settings', herf: '/setting' },
+  { id: 7, title: 'Shaphang Jongngi', herf: '/about' },
 ];
+
 export function CustomDrawerContent(props: DrawerContentComponentProps) {
   const { colorScheme } = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
@@ -59,11 +43,10 @@ export function CustomDrawerContent(props: DrawerContentComponentProps) {
 
       {menuItems.map((item) => (
         <DrawerItem
-          focused={pathname === `/${item.herf}`}
+          focused={pathname === `${item.herf}`}
           key={item.id}
           label={item.title}
-          // @ts-ignore
-          onPress={() => router.push(`/${item.herf}`)}
+          onPress={() => router.push(item.herf)}
           labelStyle={{
             textTransform: 'capitalize',
             color: isDarkMode ? colors.gray[200] : colors.gray[950],
--- src/types/MenuItems.ts
+++ src/types/MenuItems.ts
@@ -1,5 +1,7 @@
+import { Route } from 'expo-router';
+
 export type MenuItemsT = {
   id: number;
   title: string;
-  herf: string;
+  herf: Route;
 };
