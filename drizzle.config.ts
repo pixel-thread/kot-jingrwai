cbcd7c1a3af2b9bf953b409d20589472d79c4e8a
JyrwaJR <jyrwaboys@gmail.com>
2025-12-08 10:35:43 +0530

    remove sql

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  out: './src/db/drizzle/migrations',
-  schema: './src/db/drizzle/schema.ts',
-  dialect: 'sqlite',
-  driver: 'expo',
-});
