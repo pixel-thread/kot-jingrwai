a48b47b98b864fe3927540211edec81e38c1a457
JyrwaJR <jyrwaboys@gmail.com>
2025-12-15 16:03:35 +0530

    replace font awesome with material font

--- src/app/song/_layout.tsx
+++ src/app/song/_layout.tsx
@@ -1,55 +1,44 @@
 import { Stack } from 'expo-router';
 import { FloatingActionButtons } from '~/src/components/Common/FloatingActionButtons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useTextStore } from '~/src/libs/stores/text';
 import { gray } from 'tailwindcss/colors';
-import { useColorScheme } from 'nativewind';
 import { CustomHeader } from '~/src/components/Common/CustomHeader';
 import { useSongs } from '~/src/hooks/song/useSongs';
-import { Platform, ToastAndroid, TouchableOpacity, View } from 'react-native';
-import { useSongStore } from '~/src/libs/stores/songs';
-import { ThemeToggle } from '~/src/components/Common/theme/ThemeToggle';
+import { TouchableOpacity, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useColorScheme } from 'nativewind';
 
 const HeaderRight = () => {
-  const { song } = useSongs();
-  const { favoriteSongs, addFavoriteSong, removeFavoriteSong } = useSongStore();
-  const isFavorite = favoriteSongs.includes(song.metadata.number);
-
-  const onPressFavorite = () => {
-    if (isFavorite) {
-      removeFavoriteSong(song.metadata.number);
-      if (Platform.OS !== 'ios') {
-        ToastAndroid.show('Removed from favorites', ToastAndroid.SHORT);
-      }
-    } else {
-      addFavoriteSong(song.metadata.number);
-      if (Platform.OS !== 'ios') {
-        ToastAndroid.show('Added to favorites', ToastAndroid.SHORT);
-      }
-    }
-  };
+  const { colorScheme } = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+  const { increaseTextSize, decreaseTextSize } = useTextStore();
 
   return (
     <View className="flex-row items-center gap-x-4">
       <TouchableOpacity
-        onPress={onPressFavorite}
-        accessibilityLabel={isFavorite ? 'Remove from favorites' : 'Add to favorites'}>
-        <FontAwesome
-          name={isFavorite ? 'heart' : 'heart-o'}
+        onPress={increaseTextSize}
+        className="h-10 w-10 items-center justify-center rounded-full bg-gray-300/50 dark:bg-gray-600/50">
+        <MaterialCommunityIcons
+          name={'plus'}
           size={24}
-          color={isFavorite ? gray[500] : gray[500]}
+          color={isDarkMode ? gray[200] : gray[950]}
         />
       </TouchableOpacity>
-      <ThemeToggle />
+      <TouchableOpacity
+        onPress={decreaseTextSize}
+        className="h-10 w-10 items-center justify-center rounded-full bg-gray-300/50 dark:bg-gray-600/50">
+        <MaterialCommunityIcons
+          name={'minus'}
+          size={24}
+          color={isDarkMode ? gray[200] : gray[950]}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default function SongLayout() {
-  const { colorScheme } = useColorScheme();
-  const isDarkMode = colorScheme === 'dark';
-  const { song, onNextSong, onPreviousSong } = useSongs();
-  const { increaseTextSize, decreaseTextSize } = useTextStore();
+  const { song } = useSongs();
 
   return (
     <View className="flex-1" collapsable={false}>
@@ -63,39 +52,7 @@ export default function SongLayout() {
         }}
       />
 
-      <FloatingActionButtons
-        isVisible // Use state here
-        buttons={[
-          {
-            onPress: onPreviousSong,
-            icon: (
-              <FontAwesome
-                color={isDarkMode ? gray[200] : gray[950]}
-                name="chevron-left"
-                size={20}
-              />
-            ),
-          },
-          {
-            onPress: decreaseTextSize,
-            icon: <FontAwesome name="minus" size={20} color={isDarkMode ? gray[200] : gray[950]} />,
-          },
-          {
-            onPress: increaseTextSize,
-            icon: <FontAwesome color={isDarkMode ? gray[200] : gray[950]} name="plus" size={20} />,
-          },
-          {
-            onPress: onNextSong,
-            icon: (
-              <FontAwesome
-                name="chevron-right"
-                size={20}
-                color={isDarkMode ? gray[200] : gray[950]}
-              />
-            ),
-          },
-        ]}
-      />
+      <FloatingActionButtons />
     </View>
   );
 }
