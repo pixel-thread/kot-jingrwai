8ed15fa9e8bce609b46cb67a257f4d7a4819261f
JyrwaJR <jyrwaboys@gmail.com>
2025-12-16 17:14:01 +0530

    add running script in post install

--- src/libs/songs/index.ts
+++ src/libs/songs/index.ts
@@ -1,7 +1,5 @@
-import { SongT } from '~/src/types/song';
-
-// @ts-ignore
+import { SongT } from '../../types/song';
 import data from './song.json';
 
 // @ts-ignore
-export const songs: SongT[] = data;
+export const songs: SongT[] = data || [];
