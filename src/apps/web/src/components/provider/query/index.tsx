77ca49879d2d37b328cd947bd206fdebfa37499b
JyrwaJR <jyrwaboys@gmail.com>
2025-12-29 21:26:31 +0530

    only run query when is signin

--- src/apps/web/src/components/provider/query/index.tsx
+++ src/apps/web/src/components/provider/query/index.tsx
@@ -1,15 +1,22 @@
+import { useAuth } from "@clerk/nextjs";
 import {
   MutationCache,
   QueryCache,
   QueryClient,
   QueryClientConfig,
   QueryClientProvider,
-} from '@tanstack/react-query';
-const queryOptions: QueryClientConfig = {
-  queryCache: new QueryCache(),
-  mutationCache: new MutationCache(),
-};
+} from "@tanstack/react-query";
+
 export const TQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const { isSignedIn } = useAuth();
+
+  const queryOptions: QueryClientConfig = {
+    queryCache: new QueryCache(),
+    mutationCache: new MutationCache(),
+    defaultOptions: { queries: { enabled: isSignedIn } },
+  };
+
   const client = new QueryClient(queryOptions);
+
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
