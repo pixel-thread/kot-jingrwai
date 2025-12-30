141f289475cc9a12115598a12c5821783c89a972
JyrwaJR <jyrwaboys@gmail.com>
2025-12-30 20:56:15 +0530

    featured songs constant added

---
+++ src/packages/constants/src/featured-songs/index.ts
@@ -0,0 +1,16 @@
+import { FeaturedSongsI } from "@repo/types";
+
+export const featuredSongs: FeaturedSongsI[] = [
+  {
+    id: "1",
+    startDate: "2023-12-01",
+    endDate: "2023-12-31",
+    songsNo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12],
+  },
+  {
+    id: "2",
+    startDate: "2023-12-01",
+    endDate: "2023-12-31",
+    songsNo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12],
+  },
+];
