329d40064c176b2d3a4720ebe192ffd6cbb3bda0
JyrwaJR <jyrwaboys@gmail.com>
2026-01-05 18:02:58 +0530

    update

--- src/apps/mobile/eslint.config.js
+++ src/apps/mobile/eslint.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+
 import { config } from '@repo/eslint-config';
 
 module.exports = defineConfig([
