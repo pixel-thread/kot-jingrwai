345c248932e038eebb99904b5293ec1f1faabb95
JyrwaJR <jyrwaboys@gmail.com>
2026-01-06 16:10:08 +0530

    eslint test

--- src/apps/lynti-bneng/eslint.config.js
+++ src/apps/lynti-bneng/eslint.config.js
@@ -1,28 +1,28 @@
 /* eslint-env node */
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*', 'node_modules/*', '.expo'],
+    ignores: ["dist/*", "node_modules/*", ".expo"],
   },
   {
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         typescript: {
-          project: ['./tsconfig.json'],
+          project: ["./tsconfig.json"],
         },
       },
     },
   },
   {
     rules: {
-      'react/display-name': 'off',
-      'import/no-unresolved': 'off',
-      'no-unused-vars': 'warn',
-      'no-unused-expressions': 'warn',
-      'react-hooks/rules-of-hooks': 'warn',
+      "react/display-name": "off",
+      "import/no-unresolved": "off",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "react-hooks/rules-of-hooks": "warn",
     },
   },
 ]);
