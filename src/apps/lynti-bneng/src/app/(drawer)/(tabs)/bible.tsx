1ef08574183b44ee0eef5722a338f212f97fd398
JyrwaJR <jyrwaboys@gmail.com>
2026-01-10 00:03:16 +0530

    Add Page under development

--- src/apps/lynti-bneng/src/app/(drawer)/(tabs)/bible.tsx
+++ src/apps/lynti-bneng/src/app/(drawer)/(tabs)/bible.tsx
@@ -1,12 +1,6 @@
-import { Text } from '@repo/ui-native';
-import { View } from 'react-native';
+import { UnderDevelopment } from "@repo/ui-native";
+import React from "react";
 
-export default function page() {
-  return (
-    <View className={'justify-content: flex-1 items-center'}>
-      <Text size="4xl" weight={'bold'}>
-        Bible
-      </Text>
-    </View>
-  );
+export default function UnderDevelopmentPage() {
+  return <UnderDevelopment />;
 }
