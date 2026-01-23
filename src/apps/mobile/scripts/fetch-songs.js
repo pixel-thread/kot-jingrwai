2dca4347fc6cea67d087a61f9fe70c68490f9b70
JyrwaJR <jyrwaboys@gmail.com>
2026-01-23 21:51:07 +0530

    add env route

--- src/apps/mobile/scripts/fetch-songs.js
+++ src/apps/mobile/scripts/fetch-songs.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const apiBaseUrl = "http://localhost:3000/api";
+const apiBaseUrl = process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const API_URL = `${apiBaseUrl}/songs`;
 
