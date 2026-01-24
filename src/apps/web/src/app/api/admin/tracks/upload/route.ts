71f0953313608f8daa2eaf94b2b93c07745714a0
JyrwaJR <jyrwaboys@gmail.com>
2026-01-24 13:53:07 +0530

    refactoring

--- src/apps/web/src/app/api/admin/tracks/upload/route.ts
+++ src/apps/web/src/app/api/admin/tracks/upload/route.ts
@@ -1,7 +1,6 @@
-import { env } from "@/env";
-import { prisma } from "@/lib/database/prisma";
-import { supabase } from "@/lib/supabase";
 import { getUniqueSongs } from "@/services/songs/getUniqueSong";
+import { TrackService } from "@/services/track";
+import { UploadService } from "@/services/uploads/indext";
 import { handleApiErrors } from "@/utils/errors/handleApiErrors";
 import { ErrorResponse, SuccessResponse } from "@/utils/next-response";
 import { logger } from "@repo/utils";
@@ -34,22 +33,10 @@ export async function POST(request: NextRequest) {
 
     if (!songExist) return ErrorResponse({ error: "Song not found", status: 404 });
 
-    const fileExt = file.name.split(".").pop();
-    const fileName = `${crypto.randomUUID()}.${fileExt}`;
-    const filePath = `tracks/${fileName}`;
-
-    // Upload to Supabase Storage
-    console.log("Uploading Song To Supabase");
-
-    const { error: uploadError, data } = await supabase.storage
-      .from(env.SUPABASE_BUCKET)
-      .upload(filePath, Buffer.from(await file.arrayBuffer()), {
-        contentType: file.type,
-        upsert: true,
-      });
+    const { error: uploadError, data } = await UploadService.upload({ file });
 
     if (uploadError) {
-      logger.error("SUPABASE UPLOAD ERROR", { uploadError });
+      logger.error("SUPABASE Error: UPLOAD ERROR", { uploadError });
       return ErrorResponse({
         message: uploadError.message,
         error: uploadError,
@@ -57,36 +44,23 @@ export async function POST(request: NextRequest) {
       });
     }
 
-    const { data: url } = supabase.storage.from(env.SUPABASE_BUCKET).getPublicUrl(data?.path);
+    const { data: url } = TrackService.getPublicUrl(data?.path);
 
-    const trackMetadata = await prisma.trackMetadata.create({
-      data: {
+    const track = await TrackService.createTrack({
+      data: { songId },
+      metadata: {
         supabaseId: data?.id,
-        path: filePath,
+        path: data.path,
         fileName: file.name,
-        fullPath: data?.path,
+        fullPath: data?.fullPath,
         downloadUrl: url.publicUrl,
         fileSize: file.size,
         mimeType: file.type,
       },
     });
 
-    const track = await prisma.track.create({
-      data: {
-        songs: { connect: { id: songId } },
-        metadata: { connect: { id: trackMetadata.id } },
-      },
-      include: { metadata: true },
-    });
-
-    await prisma.trackMetadata.update({
-      where: { id: trackMetadata.id },
-      data: { trackId: track.id },
-    });
-
     return SuccessResponse({ data: track, message: "Song uploaded successfully" });
   } catch (error) {
-    console.log("Error uploading song", { error });
     return handleApiErrors(error);
   }
 }
