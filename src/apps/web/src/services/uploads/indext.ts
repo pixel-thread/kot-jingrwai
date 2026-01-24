14a11fe39ab6d46543b3dbe953405292ff315bd0
JyrwaJR <jyrwaboys@gmail.com>
2026-01-24 13:52:50 +0530

    add optional path to the props so when need to change the path location

--- src/apps/web/src/services/uploads/indext.ts
+++ src/apps/web/src/services/uploads/indext.ts
@@ -3,11 +3,14 @@ import { supabase } from "@/lib/supabase";
 
 type UploadFileProps = {
   file: File;
+  path?: string;
 };
-async function uploadFile({ file }: UploadFileProps) {
+
+async function uploadFile({ file, path = "tracks" }: UploadFileProps) {
   const fileExt = file.name.split(".").pop();
   const fileName = `${crypto.randomUUID()}.${fileExt}`;
-  const filePath = `tracks/${fileName}`;
+  const filePath = `${path}/${fileName}`;
+
   return await supabase.storage
     .from(env.SUPABASE_BUCKET)
     .upload(filePath, Buffer.from(await file.arrayBuffer()), {
