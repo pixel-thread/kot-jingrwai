d92fc5d9dd919d91e35df38b66a0daf89a9572e2
JyrwaJR <jyrwaboys@gmail.com>
2026-02-10 23:14:04 +0530

    not needed

--- src/apps/web/cypress/e2e/api/update/latest.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("GET /update/latest", () => {
-  it("returns object", () => {
-    cy.request("/update/latest").then((res) => {
-      expect(res.status).to.eq(200);
-      expect(res.body).to.have.property("data");
-    });
-  });
-});
