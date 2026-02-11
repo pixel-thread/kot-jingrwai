491ab1bbe0b7a9dfc0a1635e144b1a5a91b02d73
JyrwaJR <jyrwaboys@gmail.com>
2026-02-11 16:38:05 +0530

    implement login

--- src/apps/mobile/src/components/screen/auth/index.tsx
+++ src/apps/mobile/src/components/screen/auth/index.tsx
@@ -1,6 +1,7 @@
 import { AUTH_ENDPOINT } from "@repo/constants";
 import { LoginScreen as LScreen } from "@repo/ui-native";
 import { http } from "@repo/utils";
+import { useTokenManager } from "@repo/libs";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 import { Platform, ToastAndroid } from "react-native";
@@ -12,13 +13,19 @@ type FormValue = {
 
 export const LoginScreen = () => {
   const router = useRouter();
+  const { setTokens } = useTokenManager();
 
   const { isPending, mutate } = useMutation({
-    mutationFn: (data: FormValue) => http.post(AUTH_ENDPOINT.POST_LOGIN, data),
+    mutationFn: (data: FormValue) =>
+      http.post<{ refreshToken: string }>(AUTH_ENDPOINT.POST_LOGIN, data),
     onSuccess: (data) => {
       if (data.success) {
+        const res = data?.data;
         if (Platform.OS === "android") {
           ToastAndroid.show(data.message, ToastAndroid.SHORT);
+          if (res?.refreshToken) {
+            setTokens("", res?.refreshToken);
+          }
           return;
         }
         ToastAndroid.show(data.message, ToastAndroid.SHORT);
