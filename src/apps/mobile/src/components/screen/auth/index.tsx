66f01ac564ee6247abdd035d48e9b6ef839aa352
JyrwaJR <jyrwaboys@gmail.com>
2026-02-11 15:59:03 +0530

    implement login

--- src/apps/mobile/src/components/screen/auth/index.tsx
+++ src/apps/mobile/src/components/screen/auth/index.tsx
@@ -1,16 +1,38 @@
+import { AUTH_ENDPOINT } from "@repo/constants";
 import { LoginScreen as LScreen } from "@repo/ui-native";
+import { http } from "@repo/utils";
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
+import { Platform, ToastAndroid } from "react-native";
+
+type FormValue = {
+  email: string;
+  password: string;
+};
 
 export const LoginScreen = () => {
   const router = useRouter();
-  const isLoading = false;
 
-  const onLogin = () => {};
+  const { isPending, mutate } = useMutation({
+    mutationFn: (data: FormValue) => http.post(AUTH_ENDPOINT.POST_LOGIN, data),
+    onSuccess: (data) => {
+      if (data.success) {
+        if (Platform.OS === "android") {
+          ToastAndroid.show(data.message, ToastAndroid.SHORT);
+          return;
+        }
+        ToastAndroid.show(data.message, ToastAndroid.SHORT);
+        return;
+      }
+    },
+  });
+
+  const onLogin = (data: FormValue) => mutate(data);
 
   return (
     <LScreen
       onLogin={onLogin}
-      isLoading={isLoading}
+      isLoading={isPending}
       onSignup={() => router.push("/auth/sign-up")}
     />
   );
