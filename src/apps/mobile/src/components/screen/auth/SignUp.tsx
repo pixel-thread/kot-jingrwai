7e643492ed9339c0bd41c5d73680f560c4e363fa
JyrwaJR <jyrwaboys@gmail.com>
2026-02-11 19:44:13 +0530

    add signup page

--- src/apps/mobile/src/components/screen/auth/SignUp.tsx
+++ src/apps/mobile/src/components/screen/auth/SignUp.tsx
@@ -1,14 +1,28 @@
 import { SignupScreen } from "@repo/ui-native";
-import { http } from "@repo/utils";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { http, SignUpSchema } from "@repo/utils";
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
+import { z } from "zod";
+import { Platform, ToastAndroid } from "react-native";
+import { AUTH_ENDPOINT } from "@repo/constants";
+
+type FormValue = z.infer<typeof SignUpSchema>;
 
 export const Signup = () => {
   const router = useRouter();
+  const { isPending, mutate } = useMutation({
+    mutationFn: (data: FormValue) => http.post(AUTH_ENDPOINT.POST_REGISTER, data),
+    onSuccess: async (data) => {
+      if (Platform.OS === "android") {
+        ToastAndroid.show(data.message, ToastAndroid.SHORT);
+      }
+      return data.data;
+    },
+  });
 
-  const onSignup = () => {};
-  const isLoading = false;
+  const onSignup = (data: FormValue) => mutate(data);
+
   return (
-    <SignupScreen onSignup={onSignup} isLoading={isLoading} onLoginPress={() => router.back()} />
+    <SignupScreen onSignup={onSignup} isLoading={isPending} onLoginPress={() => router.back()} />
   );
 };
