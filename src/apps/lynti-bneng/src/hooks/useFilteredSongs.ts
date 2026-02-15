d64fbb10a884a17a460b0790694630758b77a6b7
JyrwaJR <jyrwaboys@gmail.com>
2026-02-15 22:16:12 +0530

    change song to online pick

--- src/apps/lynti-bneng/src/hooks/useFilteredSongs.ts
+++ src/apps/lynti-bneng/src/hooks/useFilteredSongs.ts
@@ -2,6 +2,8 @@ import { normalizeForSearch } from "@utils/normalizeTextForSearch";
 import { SongT } from "@repo/types";
 import { getSongs } from "@services/songs/getSongs";
 import { useQuery } from "@tanstack/react-query";
+import { http } from "@repo/utils";
+import { SONG_ENDPOINTS } from "@repo/constants";
 
 type UseFilteredSongsProps = {
   searchQuery?: string;
@@ -16,25 +18,28 @@ export const useFilteredSongs = ({
 
   const { data: dataSource = [] } = useQuery({
     queryKey: ["songs", { isKhorus }],
-    queryFn: () => getSongs({ isChorus: isKhorus }),
+    queryFn: () => http.get<SongT[]>(SONG_ENDPOINTS.GET_SONGS),
+    select: ({ data }) => data,
   });
   // If no query, return all songs from the selected source
-  if (!query) return dataSource;
+  if (!query) return dataSource || [];
 
   const normalizedQuery = normalizeForSearch(query);
 
-  return dataSource.filter(({ title, metadata, paragraphs }) => {
-    const searchFields = [
-      title,
-      metadata.author,
-      metadata.composer,
-      metadata.number?.toString(),
-      paragraphs[0]?.lines[0].text,
-    ].filter(Boolean); // Remove null/undefined values
+  return (
+    dataSource?.filter(({ title, metadata, paragraphs }) => {
+      const searchFields = [
+        title,
+        metadata.author,
+        metadata.composer,
+        metadata.number?.toString(),
+        paragraphs[0]?.lines[0].text,
+      ].filter(Boolean); // Remove null/undefined values
 
-    return searchFields.some((field) => {
-      const normalizedField = normalizeForSearch(field ?? "");
-      return normalizedField?.includes(normalizedQuery);
-    });
-  });
+      return searchFields.some((field) => {
+        const normalizedField = normalizeForSearch(field ?? "");
+        return normalizedField?.includes(normalizedQuery);
+      });
+    }) || []
+  );
 };
