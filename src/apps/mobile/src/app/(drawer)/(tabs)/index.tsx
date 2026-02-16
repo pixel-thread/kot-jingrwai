9b242da9a4ffba488461136f9156a35960305a93
JyrwaJR <jyrwaboys@gmail.com>
2026-02-16 09:21:54 +0530

    add admin layout

--- src/apps/mobile/src/app/(drawer)/(tabs)/index.tsx
+++ src/apps/mobile/src/app/(drawer)/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { SongFinderPage } from "~/src/components/Home/SongFindPage";
-import { AddSong } from "~/src/components/screen/admin/songs";
 
 export default function Home() {
-  return <AddSong />;
+  return <SongFinderPage />;
 }
