b21eb89266a54c27263a45e8f68362b3f0103e92
JyrwaJR <jyrwaboys@gmail.com>
2026-02-19 22:51:16 +0530

    dis powered x by header

--- src/apps/web/next.config.ts
+++ src/apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  poweredByHeader: false,
   experimental: {
     serverActions: {
       bodySizeLimit: "16mb", // or '10mb', '16mb' based on your needs
