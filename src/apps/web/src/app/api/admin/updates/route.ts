538e79979a1ada50fd8c1a55e1b69a0a9cd442f0
JyrwaJR <jyrwaboys@gmail.com>
2026-02-20 23:40:49 +0530

    share package

--- src/apps/web/src/app/api/admin/updates/route.ts
+++ src/apps/web/src/app/api/admin/updates/route.ts
@@ -1,3 +1,4 @@
+import { AppVersionTags } from "@/lib/database/prisma/generated/prisma";
 import { createUpdate } from "@/services/appVersion/update/createUpdate";
 import { getUpdates } from "@/services/appVersion/update/getUpdates";
 import { handleApiErrors } from "@/utils/errors/handleApiErrors";
@@ -37,10 +38,10 @@ export async function POST(request: Request) {
         version: body.version,
         title: body.title,
         description: body.description,
-        platforms: body.platforms,
-        releaseNotesUrl: body.releaseNotesUrl,
-        type: body.type,
-        tags: body.tags,
+        platforms: body.platforms as any,
+        releaseNotesUrl: body.releaseNotesUrl as any,
+        type: body.type as any,
+        tags: body.tags as any,
         minSupportedVersion: body.minSupportedVersion,
         releaseDate: new Date(),
         author: body.author,
