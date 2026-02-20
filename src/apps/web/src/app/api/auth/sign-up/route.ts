4fe973b855ede5b177be208ead6226dd0ab0f80e
JyrwaJR <jyrwaboys@gmail.com>
2026-02-20 09:25:59 +0530

    add comment for easy understand

--- src/apps/web/src/app/api/auth/sign-up/route.ts
+++ src/apps/web/src/app/api/auth/sign-up/route.ts
@@ -1,3 +1,4 @@
+import { logger } from "@repo/utils";
 import { AuthServices } from "@services/auth";
 import { withValidation } from "@src/utils/middleware/withValidiation";
 import { handleApiErrors } from "@utils/errors/handleApiErrors";
@@ -11,10 +12,13 @@ export const POST = withValidation({ body: SignUpSchema }, async ({ body }) => {
     if (body.password !== body.confirmPassword)
       return ErrorResponse({ message: "Passwords do not match", status: 400 });
 
+    // check if user already exists under the same email address
     const user = await AuthServices.getUnique({ where: { email } });
 
+    // return error if user already exists
     if (user) return ErrorResponse({ message: "User already exists", status: 400 });
 
+    // create user
     await AuthServices.create({
       email: body.email,
       name: body.name,
