751514cf4606149c62ece8c5493b92b388c223fc
JyrwaJR <jyrwaboys@gmail.com>
2026-02-22 18:56:24 +0530

    add default app constant

--- src/packages/constants/src/default.ts
+++ src/packages/constants/src/default.ts
@@ -1,2 +1,17 @@
 export const PAGE_SIZE = 20;
+
 export const DB_SEED_KEY = "is_db_seeded";
+
+export const AppPlatform = ["ANDROID", "IOS", "WEB"];
+
+export const AppUpdateType = ["PTA", "OTA"];
+
+export const AppTags = ["BETA", "STABLE", "PATCH", "BUGFIX"];
+
+export const UserRole = ["USER", "ADMIN", "SUPER_ADMIN"];
+
+export const appSource = ["KOT_JINGRWAI", "LYNTI_BNENG"];
+
+export const verseTypes = ["VERSE", "CHORUS", "BRIDGE", "INTRO", "OUTRO"];
+
+export const logsType = ["ERROR", "INFO", "WARN", "LOG"];
