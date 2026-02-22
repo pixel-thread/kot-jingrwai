fd4aa285056ac693e00ce450ddb863eddb763a1c
JyrwaJR <jyrwaboys@gmail.com>
2026-02-22 18:56:53 +0530

    use constant share package

--- src/packages/utils/src/validiations/logs/index.ts
+++ src/packages/utils/src/validiations/logs/index.ts
@@ -1,7 +1,8 @@
+import { logsType } from "@repo/constants";
 import z from "zod";
 
 export const LogSchema = z.object({
-  type: z.enum(["ERROR", "INFO", "WARN", "LOG"]),
+  type: z.enum(logsType).default("LOG"),
   content: z.string("Log content is required"),
   timestamp: z.string().refine((val) => !isNaN(Date.parse(val)), {
     message: "Invalid ISO date string",
