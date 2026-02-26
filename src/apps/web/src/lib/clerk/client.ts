62b66f767968655b29da2262f878774159f55e23
JyrwaJR <jyrwaboys@gmail.com>
2026-02-26 22:47:39 +0530

    remove not needed

--- src/apps/web/src/lib/clerk/client.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "@/env";
-import { createClerkClient } from "@clerk/backend";
-
-export const clientClerk = createClerkClient({
-  secretKey: env.CLERK_SECRET_KEY,
-  publishableKey: env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-});
