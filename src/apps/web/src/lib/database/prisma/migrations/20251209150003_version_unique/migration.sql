0a466dba991c4f3f952de1229bf93aaea919c521
JyrwaJR <jyrwaboys@gmail.com>
2026-02-27 14:32:06 +0530

    reset db

--- src/apps/web/src/lib/database/prisma/migrations/20251209150003_version_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[version]` on the table `AppVersion` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "AppVersion_version_key" ON "public"."AppVersion"("version");
--- src/apps/web/src/lib/database/prisma/migrations/20260212150808_song_source/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "AppSource" AS ENUM ('LYNTI_BNENG', 'KOT_JINGRWAI');
-
--- AlterTable
-ALTER TABLE "SongMetadata" ADD COLUMN     "source" "AppSource"[] DEFAULT ARRAY['KOT_JINGRWAI']::"AppSource"[];
