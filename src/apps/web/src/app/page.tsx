5021fdb7e7665b1cc28522539b86a6b0c2766155
JyrwaJR <jyrwaboys@gmail.com>
2026-04-19 17:51:36 +0530

    remove contact

--- src/apps/web/src/app/page.tsx
+++ src/apps/web/src/app/page.tsx
@@ -4,7 +4,6 @@ import { About } from "@/components/page/about";
 import { CTA } from "@/components/page/cta";
 import Home from "@/components/page/home";
 import { ShowCase } from "@/components/page/showcase";
-import { Contact } from "@/components/page/contact";
 import { NavBar } from "@/components/common/header";
 import { Footer } from "@/components/common/footer";
 
@@ -16,7 +15,6 @@ export default function page() {
       <CTA />
       <About />
       <ShowCase />
-      <Contact />
       <Footer />
     </>
   );
